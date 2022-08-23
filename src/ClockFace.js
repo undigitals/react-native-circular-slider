192ac3bc8f034990a541e4662f3f032f74db60d1
umid <undigitals@gmail.com>
2022-08-24 03:36:50 +0900

    Update ClockFace.js

--- src/ClockFace.js
+++ src/ClockFace.js
@@ -6,10 +6,10 @@ import PropTypes from 'prop-types'; // ES6
 
 export default class ClockFace extends PureComponent {
 
-  static propTypes = {
-    r: PropTypes.number,
-    stroke: PropTypes.string,
-  }
+//   static propTypes = {
+//     r: PropTypes.number,
+//     stroke: PropTypes.string,
+//   }
 
   render() {
     const { r, stroke } = this.props;
